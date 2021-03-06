2ec6b00c5da8c37340aa62d62344b686367765af
It_salemm <salemolorundare@gmail.com>
2021-03-05 23:22:01 -0800

    the add funtionality

--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Todo
 # Create your views here.
@@ -21,3 +21,14 @@ def details(request, todo_id):
 
     return render(request, 'details.html', context)
     
+
+def add(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        text = request.POST['text']
+
+        todo = Todo(title=title, text=text)
+        todo.save()
+        return redirect('/todo')
+    else:
+        return render(request, 'add.html')
