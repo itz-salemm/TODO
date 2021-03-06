71a3b6dccf2bace28496a349fea0a6b39655cdfd
It_salemm <salemolorundare@gmail.com>
2021-03-05 22:35:02 -0800

    adding the todo link

--- todos/views.py
+++ todos/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Todo
 # Create your views here.
 
 
 def index(request):
-
-
+    todo = Todo.objects.all()[:10]
     context = {
-
+        'todo': todo
     }
-    return HttpResponse("Hello world")
+
+    return render(request, 'index.html', context)
+
+
+def details(request, todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    context = {
+        'todo': todo
+    }
+
+    return render(request, 'details.html', context)
+    
